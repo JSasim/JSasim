8bfb30e37d53b8e6e82a4e8a9ae6177b84cd0e46
JSasim <131003455+JSasim@users.noreply.github.com>
2023-04-17 19:46:21 +0200

    Add files via upload
    A small project showing the basics of client-server.

---
+++ serverProject/ServerProtocolHandler.java
@@ -0,0 +1,9 @@
+package main;
+
+public class ServerProtocolHandler {
+	public String processMessage(String message) {
+		StringBuffer outputMessage = new StringBuffer();
+		
+		return outputMessage.toString();
+	}
+}
